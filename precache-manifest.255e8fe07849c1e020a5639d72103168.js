f6327373de2adb3806d523d58601a2244ec43d96
Bayon Forte <bayon@forteworks.com>
2018-11-17 08:51:33 -0500

    Updates

---
+++ precache-manifest.255e8fe07849c1e020a5639d72103168.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bayon/deployer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "eba354f127b38a4c25ae",
+    "url": "/bayon/deployer/static/js/runtime~main.eba354f1.js"
+  },
+  {
+    "revision": "9acab3e782fcc43319a6",
+    "url": "/bayon/deployer/static/js/main.9acab3e7.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/bayon/deployer/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "9acab3e782fcc43319a6",
+    "url": "/bayon/deployer/static/css/main.c873ba6c.chunk.css"
+  },
+  {
+    "revision": "446906ee1e725741aeb7443e7b386e40",
+    "url": "/bayon/deployer/index.html"
+  }
+];
