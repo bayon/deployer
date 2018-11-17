459f48482ff1cd735e56b321af474db68129b730
Bayon Forte <bayon@forteworks.com>
2018-11-17 14:42:09 -0500

    bar

--- src/App.js
+++ src/App.js
@@ -51,8 +51,8 @@ class App extends Component {
 
       
         
-         <p>{this.state.data}</p>
-         {console.log(this.state.data)}
+         <p>{data}</p>
+         
       </div>
       </div>
     );
